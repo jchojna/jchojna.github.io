7d8d67b2418454816aca43ffa7f001907c55700d
Jakub-Chojna <jakub.chojna@gmail.com>
2026-08-29 21:54:46 +0200

    chore: update layout

--- src/components/icons/IconsList.tsx
+++ src/components/icons/IconsList.tsx
@@ -1,12 +1,24 @@
+import clsx from 'clsx';
+
 import Icon from './Icon';
 
+import iconClasses from './Icon.module.css';
 import classes from './IconsList.module.css';
 
-const IconsList = ({ view, icons }: IconsList) => {
+const IconsList = ({ view, icons, placeholderCount = 0 }: IconsList) => {
   return (
     <ul className={classes.icons}>
       {icons.map((icon, index) => (
         <Icon key={index} view={view} details={icon} />
+      ))}
+      {Array.from({ length: placeholderCount }, (_, index) => (
+        <li
+          key={`placeholder-${index}`}
+          className={clsx(iconClasses.item, iconClasses.placeholder)}
+          aria-hidden
+        >
+          <div className={iconClasses.icon} />
+        </li>
       ))}
     </ul>
   );
